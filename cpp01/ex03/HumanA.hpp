fb4426e3b991ded817b44873e1bd0ab81ef858a5
minjacho42 <minjacho@student.42seoul.kr>
2024-04-25 19:01:26 +0900

    cpp01 ex03 initialized

---
+++ cpp01/ex03/HumanA.hpp
@@ -0,0 +1,15 @@
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+# include <string>
+# include "Weapon.hpp"
+
+class HumanA
+{
+	private:
+		Weapon weapon;
+	public:
+		void attack();
+};
+
+#endif
---
+++ cpp01/ex03/HumanB.hpp
@@ -0,0 +1,15 @@
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+# include <string>
+# include "Weapon.hpp"
+
+class HumanB
+{
+	private:
+		Weapon weapon;
+	public:
+		void attack();
+};
+
+#endif
