d1d50cd19c619b6554af1c48d249cc73e99d3792
minjacho42 <minjacho@student.42seoul.kr>
2024-04-25 18:44:10 +0900

    ex02 done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,15 @@
+# include <string>
+# include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string &stringREF = str;
+	std::cout << "MEMORY ADDRESS OF THE STRING VARIABLE >> \033[1;31m"<< &str << "\033[0m << \n";
+	std::cout << "MEMORY ADDRESS HELD BY stringPTR >> \033[1;31m"<< &stringPTR << "\033[0m << \n";
+	std::cout << "MEMORY ADDRESS HELD BY stringREF >> \033[1;31m"<< &stringREF << "\033[0m << \n";
+	std::cout << "THE VALUE OF THE STRING VARIABLE >> \033[1;31m"<< str << "\033[0m << \n";
+	std::cout << "THE VALUE POINTED TO BY stringPTR >> \033[1;31m"<< stringPTR << "\033[0m << \n";
+	std::cout << "THE VALUE POINTED TO BY stringREF >> \033[1;31m"<< stringREF << "\033[0m << \n";
+}
