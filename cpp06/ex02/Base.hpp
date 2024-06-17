2a76068cf76b115f4b1703aa4cdee6dfa2c18d09
minjacho42 <minjacho@student.42seoul.kr>
2024-06-17 11:55:33 +0900

    function location changed

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -1,18 +1,10 @@
 #ifndef BASE_HPP
 # define BASE_HPP
 
-# include <cstdlib>
-# include <ctime>
-# include <exception>
-
 class Base
 {
 	public:
-		Base();
 		virtual ~Base();
-		static Base	*generate(void);
-		static void	identify(Base *p);
-		static void	identify(Base& p);
 };
 
 #endif
